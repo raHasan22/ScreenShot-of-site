3171ea2ea3b880deb7fad5676edc460466de9b6a
Ahmad Hasan <r.ahmad.hasan.r@gmail.com>
2023-04-04 19:03:45 +0600

    Create full page screenshot added

--- src/App.js
+++ src/App.js
@@ -1,21 +1,70 @@
+import { useState } from 'react';
 import './App.css';
+import { useEffect } from 'react';
 
 function App() {
-  const dhbd = `${process.env.REACT_APP_NOT_SECRET_CODE}`
-  
+  const key = process.env.REACT_APP_API_KEY;
+  const [search, setSearch] = useState("https://www.exponentialhost.com/");
+  const [img, setImg] = useState("");
+  const [error, setError] = useState(false);
+  const [loading, setLoading] = useState(false);
+
+
+  const getScreenshots = async () =>{
+    setSearch("");
+    setError(false);
+    setLoading(true);
+    const response = await fetch(`https://api.apiflash.com/v1/urltoimage?access_key=${key}&url=${search}&full_page="true"&fresh="true"`);
+    if(response.ok){
+      setImg(response);
+      setLoading(false);
+    }
+    else{
+      setError(true);
+    }
+  }
+
+  const searchScreenshots = (e) => {
+    e.preventDefault();
+    getScreenshots();
+  }
+
+  useEffect(() => {
+    setSearch("");
+    getScreenshots();
+
+  },[])
 
   return (
     <div className="App">
       <nav>
         <div className="container">
-          <form>
-            <input type="text"/>
+          <form onSubmit={searchScreenshots}>
+            <input autoFocus type="text" value={search} onChange={e => setSearch(e.target.value)}/>
             <button type="submit">Take Screenshot</button>
           </form>
         </div>
       </nav>
       <div className="hero">
-<h2>{dhbd}</h2>
+        {!loading && !error ? 
+        (<div className="container">
+          {img && 
+          <a href={img.url} target='_blank'>
+            <img src={img.url} alt="background" />
+          </a>
+          }
+        </div>)
+         : 
+        !error && loading ? (
+          <div className="loading"></div>
+        ): error ? (
+          <div className="container">
+            <h2>Please insert a valid url</h2>
+          </div>
+        ): (
+          ""
+        )
+      }
       </div>
     </div>
   );
